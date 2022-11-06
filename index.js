ada3abaf0ee5d7ddcea35b4e9bd61238a6f4854c
Beau Lazear <beaulazear@gmail.com>
2022-11-06 13:26:05 -0500

    commit tree e60096d7e7cf9a7e5ae6edf8c74a52b0f65b34ac

--- index.js
+++ index.js
@@ -1,4 +1,71 @@
 /* Your Code Here */
+function createEmployeeRecord(arr) {
+
+    let employeeRecord = {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    };
+
+    return employeeRecord;
+}
+
+function createEmployeeRecords(arr) {
+
+    let employeeRecords = [];
+
+    arr.forEach(element => employeeRecords.push(createEmployeeRecord(element)))
+
+    return employeeRecords;
+}
+
+function createTimeInEvent(timeStamp) {
+
+    let [date, time] = timeStamp.split(" ");
+
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(time, 10),
+        date: date
+    })
+
+    return this;
+}
+
+function createTimeOutEvent(timeStamp) {
+
+    let [date, time] = timeStamp.split(" ");
+
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(time, 10),
+        date: date
+    })
+    
+    return this;
+
+}
+
+function hoursWorkedOnDate(formDate) {
+
+    let timeIn = this.timeInEvents.find(obj => obj.date === formDate);
+
+    let timeOut = this.timeOutEvents.find(obj => obj.date === formDate);
+
+    let hoursWorked = (timeOut.hour - timeIn.hour) * 0.01;
+
+    return hoursWorked;
+}
+
+function wagesEarnedOnDate(formDate) {
+
+    let wage = hoursWorkedOnDate.call(this, formDate) * this.payPerHour;
+
+    return wage;
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -21,3 +88,16 @@ const allWagesFor = function () {
     return payable
 }
 
+function findEmployeeByFirstName(srcArr, firstName) {
+    
+    let foundEmployee = srcArr.find(record => record.firstName === firstName);
+
+    return foundEmployee;
+}
+
+function calculatePayroll(recordsArr) {
+    console.log(recordsArr);
+    
+    return recordsArr.reduce((previous, current) => previous + allWagesFor.call( current), 0)
+
+}
